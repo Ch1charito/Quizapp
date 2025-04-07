3a569cdd6e1bcfb3a4e8139dd2f442fec20da7b9
Ch1charito <r.buschmann.work@gmail.com>
2025-04-07 16:50:47 +0200

    added html, css and some variables

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer bedeutet das HTML Tag &lt;a&gt;?",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3  
+    },
+    {
+        "question": "Wer ist kein Musiker?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Tim Berners-Lee",
+        "answer_3": "Lady Gaga",
+        "answer_4": "Justin Bieber",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist eine Programmier-Sprache?",
+        "answer_1": "HTML",
+        "answer_2": "CSS",
+        "answer_3": "Javascript",
+        "answer_4": "Spanisch",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer ist über 50 Jahre alt?",
+        "answer_1": "Manuel Neuer",
+        "answer_2": "Luciano",
+        "answer_3": "Arjen Robben",
+        "answer_4": "Robbie Williams",
+        "right_answer": 4
+    },
+];
